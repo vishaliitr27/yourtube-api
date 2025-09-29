dced9bb9beafce7bc86536156085917238268943
vishal kumar <vishalkumar@vishals-MacBook-Air-2.local>
2025-09-29 21:42:06 +0530

    Updated backend to use live transcript second with editing

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! this is a testing backend service';
+    return 'Hello World! this is a testing backend service after editing transcript';
   }
 }
